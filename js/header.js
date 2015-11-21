399227c0b27b22a8782de566a13cc679782cc5e5
chihsuan <chihsuan.tw@gmail.com>
2015-11-21 15:32:35 +0800

    update

--- js/header.js
+++ js/header.js
@@ -7,12 +7,12 @@
   links += '<a class="mdl-navigation__link" href="http://global.taiwanstat.com/r/">世界即時資訊</a>';
   links += '<a class="mdl-navigation__link" href="http://global.taiwanstat.com/l/">世界統計資訊</a>';
   links += '<a class="mdl-navigation__link" href="http://taiwanstat.com/opendata">開放資料分析部落格</a>';
-  links += '<a class="mdl-navigation__link" href="https://www.facebook.com/taiwanstat">Facebook 粉專</a>';
+  links += '<a class="mdl-navigation__link" href="http://taiwanstat-about-us.cannerapp.com/">關於我們</a>';
 
 
 	var str = '<header class="mdl-layout__header">';
   str += '<div class="mdl-layout__header-row">';
-  str += '<span class="mdl-layout-title"><a href="http://real.taiwanstat.com/" style="color: #FFF"><img src="/assets/images/round-logo.png" id="round-logo"/>用數據看台灣</a></span>';
+  str += '<span class="mdl-layout-title"><a href="http://real.taiwanstat.com/"><img src="/assets/images/round-logo.png" id="round-logo"/>用數據看台灣</a></span>';
   str += '<div class="mdl-layout-spacer"></div>';
   str += '<nav class="mdl-navigation mdl-layout--large-screen-only">';
   str += links;
